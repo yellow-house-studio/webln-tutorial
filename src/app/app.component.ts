bd2c993de00107ce1e0744886b72746858d6d210
Johanna Hultén <min.mail@johannap.se>
2022-09-13 11:02:58 +0200

    Start of tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { GetInfoResponse, requestProvider, WebLNProvider } from 'webln';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'webln-tutorial';
+  public webLnEnabled: boolean = false;
+  public nodeInfo: GetInfoResponse | undefined = undefined;
+
+  public weblnProvider: WebLNProvider | undefined = undefined;
+
+  public async connectProvider(): Promise<void> {
+    try {
+      this.weblnProvider = await requestProvider();
+      console.log(this.weblnProvider);
+    } catch (error) {
+    }
+  }
+
+  public checkEnabled(): void {
+    this.weblnProvider?.enable().then(() => {
+      this.webLnEnabled = true;
+    }).catch(() => {
+      this.webLnEnabled = false;
+    });
+  }
+
+  public async getInfo(): Promise<void> {
+    try {
+      this.nodeInfo = await this.weblnProvider?.getInfo();
+    } catch (error) {
+
+    }
+  }
+
+  public makeInvoice(): void {
+
+  }
+
+  public sendPayment(): void {
+
+  }
+
+  public keySend(): void {
+
+  }
 }
